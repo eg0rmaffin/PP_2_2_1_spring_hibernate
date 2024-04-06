906ee42b5822ca25651a7d29554afb65a25277bf
Egor Markin <eg0rmaffin@gmail.com>
2024-04-06 20:03:38 +0300

    added car entity db func

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,18 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      Car car1 = new Car("Model1", 2022);
+      user1.setCar(car1);
+      car1.setUser(user1);
+      userService.add(user1);
+
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      Car car2 = new Car("Model2", 2021);
+      user2.setCar(car2);
+      car2.setUser(user2);
+      userService.add(user2);
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +35,14 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = "+user.getCar());
          System.out.println();
       }
+
+      String model = "Model1";
+      int series = 2022;
+      User userByCar = userService.getUserByCarModelAndSeries(model, series);
+      System.out.println("User owning car with model " + model + " and series " + series + ": " + userByCar);
 
       context.close();
    }
--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -26,4 +26,14 @@ public class UserDaoImp implements UserDao {
       return query.getResultList();
    }
 
+   @Override
+   public User getUserByCarModelAndSeries(String model, int series) {
+      TypedQuery<User> query = sessionFactory.getCurrentSession()
+              .createQuery("select u from User u where u.car.model = :model and u.car.series = :series", User.class)
+              .setParameter("model", model)
+              .setParameter("series", series)
+              .setMaxResults(1);
+      return query.getSingleResult();
+   }
+
 }
