56d2a579b03dcab1134f3c7e09da8ef881bfa6d2
Egor Markin <eg0rmaffin@gmail.com>
2024-04-26 13:24:16 +0300

    fixed issues

--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -1,6 +1,7 @@
 package hiber.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
@@ -69,4 +70,19 @@ public class User {
    public void setCar(Car car) {
       this.car = car;
    }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      User user = (User) o;
+      return Objects.equals(id, user.id) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(email, user.email) && Objects.equals(car, user.car);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(id, firstName, lastName, email);
+   }
+
+
 }
